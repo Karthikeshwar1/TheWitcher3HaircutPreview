2f96ca8fa32c8648e61a09369fdb4c02273e64bc
Karthikeshwar <43902130+Karthikeshwar1@users.noreply.github.com>
2022-01-20 20:59:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-let hairStyle = "lgtbh"; let mustacheStyle = "cs"; const radioButtons_hair = document.querySelectorAll('input[name="hair"]'); const radioButtons_mustache = document.querySelectorAll('input[name="mustache"]'); for (const radioButton of radioButtons_hair) { radioButton.addEventListener("change", showSelectedHair) } for (const radioButton of radioButtons_mustache) { radioButton.addEventListener("change", showSelectedMustache) } function showSelectedHair(e) { console.log(e); console.log(String(this.value)); hairStyle = String(this.value); showPreview(hairStyle, mustacheStyle) } function showSelectedMustache(e) { console.log(e); mustacheStyle = String(this.value); showPreview(hairStyle, mustacheStyle) } function showPreview(h, m) { console.log(h); console.log(h + "_" + m + ".png"); document.getElementById("preview").src = String("./img/" + h + "_" + m + "_C.png") }
+let hairStyle = "lgtbh";
+let mustacheStyle = "cs";
+// const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+// generate the radio groups        
+// const group = document.querySelector("#group");
+// group.innerHTML = sizes.map((size) => `<div>
+//     <input type="radio" name="size" value="${size}" id="${size}">
+//      <label for="${size}">${size}</label>
+// </div>`).join(' ');
+
+// add an event listener for the change event
+const radioButtons_hair = document.querySelectorAll('input[name="hair"]');
+const radioButtons_mustache = document.querySelectorAll('input[name="mustache"]');
+for (const radioButton of radioButtons_hair) {
+    radioButton.addEventListener('change', showSelectedHair);
+}
+for (const radioButton of radioButtons_mustache) {
+    radioButton.addEventListener('change', showSelectedMustache);
+}
+
+function showSelectedHair(e) {
+    console.log(e);
+    // if (this.checked) {
+    //     document.querySelector('#output_hair').innerText = `You selected ${this.value}`;
+    // }
+    console.log(String(this.value));
+    hairStyle = String(this.value);
+    showPreview(hairStyle, mustacheStyle);
+}
+function showSelectedMustache(e) {
+    console.log(e);
+    // if (this.checked) {
+    //     document.querySelector('#output_mustache').innerText = `You selected ${this.value}`;
+    // }
+    mustacheStyle = String(this.value);
+    showPreview(hairStyle, mustacheStyle);
+
+}
+
+function showPreview(h, m) {
+    console.log(h);
+    console.log(h + '_' + m + '.png');
+    document.getElementById('preview').src = String('./img/' + h + '_' + m + '_C.png');
+}
