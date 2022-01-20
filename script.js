dc8ff97b9020d14bdd3f59400b509e1ecf364650
Karthikeshwar <43902130+Karthikeshwar1@users.noreply.github.com>
2022-01-20 21:09:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 let hairStyle = "lgtbh";
 let mustacheStyle = "cs";
-// const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
-// generate the radio groups        
-// const group = document.querySelector("#group");
-// group.innerHTML = sizes.map((size) => `<div>
-//     <input type="radio" name="size" value="${size}" id="${size}">
-//      <label for="${size}">${size}</label>
-// </div>`).join(' ');
-
-// add an event listener for the change event
 const radioButtons_hair = document.querySelectorAll('input[name="hair"]');
 const radioButtons_mustache = document.querySelectorAll('input[name="mustache"]');
 for (const radioButton of radioButtons_hair) {
@@ -21,18 +12,14 @@ for (const radioButton of radioButtons_mustache) {
 
 function showSelectedHair(e) {
     console.log(e);
-    // if (this.checked) {
-    //     document.querySelector('#output_hair').innerText = `You selected ${this.value}`;
-    // }
+   
     console.log(String(this.value));
     hairStyle = String(this.value);
     showPreview(hairStyle, mustacheStyle);
 }
 function showSelectedMustache(e) {
     console.log(e);
-    // if (this.checked) {
-    //     document.querySelector('#output_mustache').innerText = `You selected ${this.value}`;
-    // }
+   
     mustacheStyle = String(this.value);
     showPreview(hairStyle, mustacheStyle);
 
